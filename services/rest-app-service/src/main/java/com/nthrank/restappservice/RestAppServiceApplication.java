204f506a382e2a01eef59a6c7ff8bc72afdb48cd
gnarendra89 <gnarendra89@gmail.com>
2021-06-12 10:10:56 +0530

    Rest Service to call other services. Please note this service will also be registered as eureka client only

---
+++ services/rest-app-service/src/main/java/com/nthrank/restappservice/RestAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nthrank.restappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAppServiceApplication.class, args);
+	}
+
+}
---
+++ services/rest-app-service/src/test/java/com/nthrank/restappservice/RestAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nthrank.restappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
